b1c5727eef85f1ea41f4c8ed868a4ca899858a93
Leo <syt09159@gmail.com>
2026-03-25 19:24:30 +0900

    CSS and js

---
+++ step03_JavaScript/js/basic.js
@@ -0,0 +1,19 @@
+//여기는 자바스크립 영역입니댜.
+
+/*여러 줄 또는부분 주석입니다.*/
+
+//콘솔에 출력
+console.log("어디에 출력되나요?");
+
+//브라우저에 출력
+document.write("<h1>난 어디에 나오나요?</h1>");
+
+//함수 작성
+function test() {
+  window.alert("되나?"); // 메시지 창
+  //h2에 css 적용
+
+  //elementf를 찾는 방법
+  document.getElementById("h2").style.backgroundColor = "red";
+  document.getElementById("h2").style.color = "white";
+}
